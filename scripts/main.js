9a898a11112333cd4f3f84ee50e31931e610fee4
Gorodetskaya-Mariia <41908932+Gorodetskaya-Mariia@users.noreply.github.com>
2018-09-01 15:20:55 +0500

    закончена гл стр и мод.окно

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+var link = document.querySelector(".search-hotels__button");
+var popup = document.querySelector(".modal-search-hotels");
+var isStorageSupport = true;
+var storage="";
+
+try {
+  storage = localStorage.getItem("userName");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
