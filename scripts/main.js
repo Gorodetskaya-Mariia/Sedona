34f0317d4343386724ba2f7cacf590aad0a7a8a9
Gorodetskaya-Mariia <41908932+Gorodetskaya-Mariia@users.noreply.github.com>
2018-09-03 23:38:06 +0500

    модернизация мод.окно

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,12 @@ link.addEventListener("click", function (evt) {
 
 window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
+    popup.classList.remove("modal-show");
+  }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
